3794eb7f870e086cfa995cad1ff099a339122864
natively.dev <api@natively.dev>
2026-02-02 06:58:05 +0000

    style: Enlarge logo on auth screen to 240x240

--- app/auth.tsx
+++ app/auth.tsx
@@ -192,8 +192,8 @@ const styles = StyleSheet.create({
     marginBottom: 32,
   },
   logo: {
-    width: 120,
-    height: 120,
+    width: 240,
+    height: 240,
     marginBottom: 16,
   },
   appName: {
